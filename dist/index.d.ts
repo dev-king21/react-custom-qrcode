d70554f11ee3da14af49c059de76dc5bbd63e691
dev-king21 <goodhelper21@gmail.com>
2022-03-31 17:15:55 +0900

    added eye colors of qrcode into gcoro/react-qrcode-logo

---
+++ dist/index.d.ts
@@ -0,0 +1,61 @@
+import * as React from 'react';
+declare type CornerRadii = number | number[] | InnerOuterRadii;
+declare type InnerOuterRadii = {
+    inner: number | number[];
+    outer: number | number[];
+};
+declare type Colors = string | string[];
+export interface IProps {
+    value?: string;
+    ecLevel?: 'L' | 'M' | 'Q' | 'H';
+    enableCORS?: boolean;
+    size?: number;
+    quietZone?: number;
+    bgColor?: string;
+    fgColor?: string;
+    logoImage?: string;
+    logoWidth?: number;
+    logoHeight?: number;
+    logoOpacity?: number;
+    removeQrCodeBehindLogo?: boolean;
+    eyeRadius?: CornerRadii | CornerRadii[];
+    eyeColor?: Colors;
+    qrStyle?: 'squares' | 'dots';
+    style?: object;
+    id?: string;
+}
+export declare class QRCode extends React.Component<IProps, {}> {
+    private canvas;
+    static defaultProps: IProps;
+    private static utf16to8;
+    /**
+     * Draw a rounded square in the canvas
+     */
+    private drawRoundedSquare;
+    /**
+     * Draw a single positional pattern eye.
+     */
+    private drawPositioningPattern;
+    /**
+     * Is this dot inside a positional pattern zone.
+     */
+    private isInPositioninZone;
+    private transformPixelLengthIntoNumberOfCells;
+    private isCoordinateInImage;
+    constructor(props: IProps);
+    shouldComponentUpdate(nextProps: IProps): boolean;
+    componentDidMount(): void;
+    componentDidUpdate(): void;
+    update(): void;
+    render(): React.DetailedReactHTMLElement<{
+        id: string;
+        height: number;
+        width: number;
+        style: {
+            height: string;
+            width: string;
+        };
+        ref: React.RefObject<HTMLCanvasElement>;
+    }, HTMLCanvasElement>;
+}
+export {};
